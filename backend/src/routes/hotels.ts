04f08213f713282de13255a4d578a615fa5be3b0
danieliyahu1 <danieliyahu1@gmail.com>
2024-08-12 16:23:59 +0300

    deleted Link and Navigate from GuestInfoForm

--- backend/src/routes/hotels.ts
+++ backend/src/routes/hotels.ts
@@ -1,8 +1,11 @@
 import express, {Response, Request} from "express";
 import Hotel from "../models/hotel";
 import { HotelSearchResponse } from "../shared/types";
+import { param, validationResult } from "express-validator";
 
 const router = express.Router();
+
+
 
 router.get("/search", async (req: Request, res: Response)=> {
     try{
@@ -25,6 +28,7 @@ router.get("/search", async (req: Request, res: Response)=> {
         const pageSize = 5;
         const pageNumber = parseInt(req.query.page ? req.query.page.toString() : "1");
         const skip = (pageNumber - 1) * pageSize;
+
         const hotels = await Hotel.find(query).sort(sortOptions).skip(skip).limit(pageSize);
 
         const total = await Hotel.countDocuments(query);
@@ -44,7 +48,26 @@ router.get("/search", async (req: Request, res: Response)=> {
         console.log("error", error);
         res.status(500).json({message: "Somethug went wrong"});
     }
-})
+});
+
+router.get("/:id", 
+    [param("id").notEmpty().withMessage("Hotel Id is required")],
+     async(req: Request, res: Response) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) {
+            return res.status(400).json({errors: errors.array()});
+        }
+
+        const id = req.params.id.toString();
+
+        try {
+            const hotel = await Hotel.findById(id);
+            res.json(hotel);
+        } catch(error){
+            console.log(error);
+            res.status(500).json({message: "Error fetching hotel"})
+        }
+     });
 
 const constructSearchQuery = (queryParams: any) => {
     let constructedQuery: any = {};
--- e2e-tests/tests/search-hotels.spec.ts
+++ e2e-tests/tests/search-hotels.spec.ts
@@ -24,5 +24,15 @@ test("Should show hotel search results", async({page}) => {
 
     await expect(page.getByText("Hotels found in israel")).toBeVisible();
     await expect(page.getByText("Gedera Hostel")).toBeVisible();
-
 })
+
+test("should show hotel detail", async({page}) => {
+    await page.goto(UI_URL);
+    await page.getByPlaceholder("Where are you going?").fill("israel");
+    await page.getByRole('button', { name: 'Search' }).click();
+
+    await page.getByText("Gedera Hostel").click();
+    await expect(page).toHaveURL(/detail/);
+    await expect(page.getByRole("button", {name: "Book now"})).toBeVisible();
+
+});
